312bfcacc12405a07c25c4839ce2b9d8fc8a48b5
Rachel <raa563@nyu.edu>
2023-10-26 19:00:15 -0400

    Add summary and notes to SingleBook page

--- client/src/components/pages/SingleBook.js
+++ client/src/components/pages/SingleBook.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import './pagestyles/SingleBook.css'
 
 function SingleBook() {
   const { bookid } = useParams();
   const [book, setBook] = useState({});
+  const [textDisplay, setTextDisplay] = useState("");
 
   useEffect(() => {
     fetch('/getbook', {
@@ -15,13 +17,43 @@ function SingleBook() {
         bookid: bookid
       })
     }).then(response => response.json())
-    .then(data => setBook(data));
+    .then(data => setBook(data))
+    .then(setTextDisplay(book.summary));
 
   },[bookid]);
 
+  function summaryClick(event) {
+    event.preventDefault();
+    setTextDisplay(book.summary);
+  }
+
+  function notesClick(event) {
+    event.preventDefault();
+    setTextDisplay(book.notes);
+  }
+
   return (
-    <div className='page-content'>
-      <h1>{book.title}</h1>
+    <div className='page-content left-right'> 
+    {book ? 
+      <>
+        <div className="left">
+          <h1>{book.title}</h1>
+          <p>Author: {book.authors}</p>
+          <p>Genres: {book.genres}</p>
+          <h2>Included in...</h2>
+          {book.collections.map(col => {
+            return( <p>{col.name}</p> )
+          })}
+        </div>
+        <div className="right">
+          <div className="switch">
+            <button className="switch-btn"onClick={summaryClick}>SUMMARY</button>
+            <button className="switch-btn"onClick={notesClick}>NOTES</button>
+          </div>
+          <p className="summary-notes">{textDisplay}</p>
+        </div>
+      </>
+    : null}
     </div>
 
   )
